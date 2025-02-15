04a76ff8b115bf1f21920c1fd6eb49b25ef81b70
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2025-02-15 19:31:48 +0530

    testing 2

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -19,10 +19,9 @@ const Widget = ({ projectId }) => {
   const submit = async (e) => {
     e.preventDefault();
     const form = e.target;
-    const project_id = String(projectId);
-        console.log(projectId)
+
     const data = {
-      p_project_id: project_id,
+      p_project_id: "1ca117cf-8b15-4121-8e32-22167b0f92ff",
       p_user_name: form.name.value,
       p_user_email: form.email.value,
       p_message: form.feedback.value,
