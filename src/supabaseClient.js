1a300e0b3a5b8296560a92a7e4259653a19c89c9
adarsh-vishwakarma <adarshvishwakarma704@gmail.com>
2025-02-15 20:25:17 +0530

    testing 3

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://bxqcrbukcmqroohyiyih.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Create a single supabase client for interacting with your database
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
 export default supabase;
